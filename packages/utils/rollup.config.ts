80f0c1cddfcd330405f0b025ab5368903a834698
evan-moon <bboydart91@gmail.com>
2021-07-30 16:48:48 +0900

    chore: publish test

--- packages/utils/rollup.config.ts
+++ packages/utils/rollup.config.ts
@@ -1,13 +1,12 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
-import sourceMaps from 'rollup-plugin-sourcemaps';
 import typescript from 'rollup-plugin-typescript2';
 import json from 'rollup-plugin-json';
 import packageJSON from './package.json';
 
 export default {
   input: `src/index.ts`,
-  output: [{ dir: packageJSON.module, format: 'cjs', sourcemap: true }],
+  output: [{ dir: packageJSON.publishConfig.module, format: 'cjs', sourcemap: true }],
   external: [],
   watch: {
     include: 'src/**',
@@ -19,6 +18,5 @@ export default {
     commonjs({
       include: 'node_modules/**',
     }),
-    sourceMaps(),
   ],
 };
