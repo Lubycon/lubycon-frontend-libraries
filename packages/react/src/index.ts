40edd16e8d270a3ecc90d8010d30dfe8a0d8a279
SONGMINSEOK <somony9292@gmail.com>
2021-08-02 10:44:31 +0900

    feat: useWindow window size hooks (#16)
    * feat: useWindow window size hooks
    
    * fix: renamed useWindow to useWindowSize

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -2,3 +2,4 @@ export { default as useBooleanState } from './hooks/useBooleanState';
 export { default as usePreviousState } from './hooks/usePreviousState';
 export { default as useThrottle } from './hooks/useThrottle';
 export { default as useDebounce } from './hooks/useDebounce';
+export { default as useWindow } from './hooks/useWindowSize';
