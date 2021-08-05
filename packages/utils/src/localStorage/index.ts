c4bafe4bb25f15d71e207aaad3e46105809c8f97
geonwoo.jeong <doscmDev@gmail.com>
2021-08-05 23:50:24 +0900

    refactor(utils): bind localStorage event (#19)
    * refactor: bind localStorage event
    
    * refactor: rename LocalStorageChanged to LocalStorageChangeEvent
    
    * refactor: separate LocalStorageChangeEvent & merge 'export'

--- packages/utils/src/localStorage/index.ts
+++ packages/utils/src/localStorage/index.ts
@@ -1,36 +1,12 @@
 import formatISO from 'date-fns/formatISO';
 import addHours from 'date-fns/addHours';
 import isBefore from 'date-fns/isBefore';
-import { isServer } from '../constants/env';
+import { storage } from './storage';
+import { LocalStorageChangeEvent } from './localStorageEvent';
 
 interface LubyconStorageData<T> {
   data: T;
   expiry: string;
-}
-
-function canUseStorage() {
-  if (isServer === true) {
-    return false;
-  }
-
-  let storage;
-  try {
-    storage = window.localStorage;
-    const testItem = '__storage_test__';
-    storage.setItem(testItem, testItem);
-    storage.removeItem(testItem);
-    return true;
-  } catch (e) {
-    return (
-      e instanceof DOMException &&
-      (e.code === 22 ||
-        e.code === 1014 ||
-        e.name === 'QuotaExceededError' ||
-        e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
-      storage &&
-      storage.length > 0
-    );
-  }
 }
 
 function getExpiry(expiryHour?: number) {
@@ -46,37 +22,24 @@ function isLubyconUtilsItem<T>(item: LubyconStorageData<T> | T): item is Lubycon
   return 'data' in item && 'expiry' in item;
 }
 
-function printNoStorageWarningLog() {
-  console.warn('로컬스토리지를 사용할 수 없는 환경입니다');
-}
-
 /**
  * 로컬스토리지에 데이터를 저장합니다. 3번째 인자 expiryHour로 데이터의 만료 시간을 지정할 수 있습니다.
  */
-export function setLocalStorageItem<T>(key: string, data: T, expiryHour?: number) {
-  if (!canUseStorage()) {
-    printNoStorageWarningLog();
-    return;
-  }
-
+function setLocalStorageItem<T>(key: string, data: T, expiryHour?: number) {
   const payload: LubyconStorageData<T> = {
     data,
     expiry: getExpiry(expiryHour),
   };
 
-  window.localStorage.setItem(key, JSON.stringify(payload));
+  storage.setItem(key, JSON.stringify(payload));
+  window.dispatchEvent(new LocalStorageChangeEvent({ key, data }));
 }
 
 /**
  * 로컬스토리지에서 데이터를 가져옵니다. 만약 만료 시간이 지정된 데이터이고, 만료 시간이 지난 상태라면 null이 반환됩니다.
  */
-export function getLocalStorageItem<T>(key: string): T | null {
-  if (!canUseStorage()) {
-    printNoStorageWarningLog();
-    return null;
-  }
-
-  const payload = window.localStorage.getItem(key);
+function getLocalStorageItem<T>(key: string): T | null {
+  const payload = storage.getItem(key);
   if (payload == null) {
     return null;
   }
@@ -89,7 +52,7 @@ export function getLocalStorageItem<T>(key: string): T | null {
     if (isNaN(expiry.getTime()) || isBefore(now, expiry)) {
       return parsedPayload.data;
     } else {
-      window.localStorage.removeItem(key);
+      storage.removeItem(key);
       return null;
     }
   } else {
@@ -100,19 +63,15 @@ export function getLocalStorageItem<T>(key: string): T | null {
 /**
  * 로컬스토리지에서 데이터를 제거합니다.
  */
-export function removeLocalStorageItem(key: string) {
-  if (!canUseStorage()) {
-    printNoStorageWarningLog();
-    return;
-  }
-
-  window.localStorage.removeItem(key);
+function removeLocalStorageItem(key: string) {
+  storage.removeItem(key);
+  window.dispatchEvent(new LocalStorageChangeEvent({ key, data: null }));
 }
 
 /**
  * 로컬스토리지에서 데이터를 가져온 후 해당 데이터를 로컬스토리지에서 제거합니다.
  */
-export function popLocalStorageItem<T>(key: string): T | null {
+function popLocalStorageItem<T>(key: string): T | null {
   const data = getLocalStorageItem<T>(key);
   removeLocalStorageItem(key);
   return data;
@@ -121,11 +80,15 @@ export function popLocalStorageItem<T>(key: string): T | null {
 /**
  * 로컬스토리지 내의 모든 데이터를 제거합니다.
  */
-export function clearLocalStorage() {
-  if (!canUseStorage()) {
-    printNoStorageWarningLog();
-    return;
-  }
-
-  window.localStorage.clear();
+function clearLocalStorage() {
+  storage.clear();
 }
+
+export {
+  setLocalStorageItem,
+  getLocalStorageItem,
+  removeLocalStorageItem,
+  popLocalStorageItem,
+  clearLocalStorage,
+  LocalStorageChangeEvent,
+};
