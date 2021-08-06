ccc689ecb68e6c5bb16e700c071f124dc1304eae
evan-moon <bboydart91@gmail.com>
2021-08-06 15:27:41 +0900

    remove test file

--- packages/mattermost/test/index.js
+++
@@ -1,9 +0,0 @@
-const { MattermostClient } = require('../dist');
-const client = new MattermostClient('3fhez5bubtdsdf8sejicrkouny');
-
-async function main() {
-  const response = await client.fetchAllUsers({});
-  console.log(response);
-}
-
-main();
