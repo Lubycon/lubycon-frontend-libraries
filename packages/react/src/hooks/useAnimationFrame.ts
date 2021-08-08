f15308e47ada8f2e1d1e622ecd71e88297a3b029
Evan Moon <bboydart91@gmail.com>
2021-08-08 14:50:44 +0900

    feat(react): useAnimationFrame 훅 추가 (#30)
    * feat(react): useAnimationFrame 훅 추가
    
    * remove console log

---
+++ packages/react/src/hooks/useAnimationFrame.ts
@@ -0,0 +1,31 @@
+import { useEffect, useRef } from 'react';
+
+/**
+ * requestAnimationFrame을 쉽게 사용할 수 있는 훅입니다.
+ * 콜백의 레퍼런스가 변경되면 기존의 animationFrame을 취소하고 새로운 animationFrame을 등록하니, 최적화를 위해 콜백의 레퍼런스 관리를 신경써주세요.
+ * @example
+ * const [progress, setProgress] = useState(0);
+ * const updateProgress = useCallback(() => {
+ *   setProgress(prevProgress => prevProgress >= 100 ? 0 : prevProgress + 1);
+ * }, []);
+ *
+ * useAnimationFrame(updateProgress);
+ */
+export function useAnimationFrame(callback: () => void) {
+  const animateRequestRef = useRef<number>();
+
+  useEffect(() => {
+    const animate = () => {
+      callback();
+      animateRequestRef.current = requestAnimationFrame(animate);
+    };
+
+    animate();
+
+    return () => {
+      if (animateRequestRef.current != null) {
+        cancelAnimationFrame(animateRequestRef.current);
+      }
+    };
+  }, [callback]);
+}
