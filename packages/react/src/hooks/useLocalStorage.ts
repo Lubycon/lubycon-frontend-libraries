854dc776b40afec03a6a20717c7606257b6312f0
geonwoo.jeong <doscmdev@gmail.com>
2021-08-10 18:13:08 +0900

    feat: add useLocalStorage

---
+++ packages/react/src/hooks/useLocalStorage.ts
@@ -0,0 +1,71 @@
+import {
+  setLocalStorageItem,
+  removeLocalStorageItem,
+  LocalStorageChangeEvent,
+} from '@lubycon/utils';
+
+import { useEffect, useState, useCallback } from 'react';
+
+function tryParse(value: string) {
+  try {
+    return JSON.parse(value);
+  } catch {
+    return value;
+  }
+}
+
+function isTypeOfLocalStorageChangeEvent<TValue>(evt: any): evt is LocalStorageChangeEvent<TValue> {
+  return !!evt && evt.type === LocalStorageChangeEvent.eventName;
+}
+
+export type LocalStorageNullableReturnValue<T> = [
+  T | null,
+  (newValue: T | null) => void,
+  () => void
+];
+export type LocalStorageReturnValue<T> = [T, (newValue: T | null) => void, () => void];
+
+export function useLocalStorage<T = string>(key: string): LocalStorageNullableReturnValue<T>;
+export function useLocalStorage<T = string>(
+  key: string,
+  defaulValue: T
+): LocalStorageReturnValue<T>;
+export function useLocalStorage<T = string>(key: string, defaulValue: T | null = null) {
+  const [localState, updateLocalState] = useState<T | null>(
+    localStorage.getItem(key) === null ? defaulValue : tryParse(localStorage.getItem(key)!)
+  );
+
+  const onLocalStorageChange = (event: LocalStorageChangeEvent<T> | StorageEvent) => {
+    if (isTypeOfLocalStorageChangeEvent<T>(event)) {
+      if (event.detail.key === key) {
+        updateLocalState(event.detail.data);
+      }
+    } else {
+      if (event.key === key) {
+        updateLocalState(event.newValue === null ? null : tryParse(event.newValue));
+      }
+    }
+  };
+
+  useEffect(() => {
+    const listener = (e: Event) => onLocalStorageChange(e as LocalStorageChangeEvent<T>);
+    window.addEventListener(LocalStorageChangeEvent.eventName, listener);
+
+    window.addEventListener('storage', listener);
+
+    if (localStorage.getItem(key) === null && defaulValue !== null) {
+      setLocalStorageItem(key, defaulValue);
+    }
+
+    return () => {
+      window.removeEventListener(LocalStorageChangeEvent.eventName, listener);
+      window.removeEventListener('storage', listener);
+    };
+  }, [key]);
+
+  const writeState = useCallback((value: T) => setLocalStorageItem(key, value), [key]);
+  const deleteState = useCallback(() => removeLocalStorageItem(key), [key]);
+  const state: T | null = localState ?? defaulValue;
+
+  return [state, writeState, deleteState];
+}
