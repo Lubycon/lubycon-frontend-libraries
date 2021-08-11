25157c9fd662aa209c6d82520afba41e3f942ffb
geonwoo.jeong <doscmdev@gmail.com>
2021-08-12 01:34:42 +0900

    refactor: delete polyfill for CustomEvent

--- packages/utils/src/localStorage/localStorageEvent.ts
+++ packages/utils/src/localStorage/localStorageEvent.ts
@@ -1,32 +1,3 @@
-/**
- * CustomEvent polyfill 적용: https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent/CustomEvent
- */
-(() => {
-  if (typeof window === 'undefined') {
-    window = {} as unknown as Window & typeof globalThis;
-  }
-
-  if (typeof window.CustomEvent === 'function') {
-    return;
-  }
-
-  function CustomEvent<T>(
-    typeArg: string,
-    params: CustomEventInit<T> = { bubbles: false, cancelable: false }
-  ): CustomEvent<T> {
-    const evt = document.createEvent('CustomEvent');
-    evt.initCustomEvent(
-      typeArg,
-      params?.bubbles ?? false,
-      params?.cancelable ?? false,
-      params?.detail
-    );
-    return evt;
-  }
-
-  window.CustomEvent = CustomEvent as unknown as typeof window.CustomEvent;
-})();
-
 export interface LocalStorageEventPayload<T> {
   key: string;
   data: T;
