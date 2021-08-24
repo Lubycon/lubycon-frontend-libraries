01827b64e4700e10ea752c824d7d35057f7dca72
evan-moon <bboydart91@gmail.com>
2021-08-24 22:56:49 +0900

    jsDoc 추가

--- packages/react/src/components/ImpressionArea.tsx
+++ packages/react/src/components/ImpressionArea.tsx
@@ -10,6 +10,14 @@ interface Props {
   children: ReactNode;
 }
 
+/**
+ * 요소가 화면 안에 등장하거나 화면에서 사라지면 onImpressionStart, onImpressionEnd 이벤트를 트리거하는 컴포넌트입니다.
+ *
+ * @example
+ * <ImpressionArea onImpressionStart={() => console.log('나타났다!')}>
+ *   <div />
+ * </ImpressionArea>
+ */
 const ImpressionArea = forwardRef(
   ({ children, ...rest }: Props, forwardedRef: Ref<HTMLDivElement>) => {
     const impressionRef = useImpression({
--- packages/react/src/components/LoggingImpression.tsx
+++ packages/react/src/components/LoggingImpression.tsx
@@ -11,6 +11,19 @@ interface Props {
   impressionOptions: ComponentProps<typeof ImpressionArea>;
 }
 
+/**
+ * \@lubycon/logger를 선언적으로 사용할 수 있는 컴포넌트입니다.
+ * 내부적으로 ImpressionArea를 사용하고 있기 때문에 자동으로 onImpressionStart 이벤트 발생 시 로거가 트리거링됩니다.
+ *
+ * @example
+ * <LoggingImpression
+ *   view="구독_페이지"
+ *   logName="구독해제_버튼노출"
+ *   params={{ 서비스: service.name }}
+ * >
+ *   <button>구독 취소</button>
+ * </LoggingImpression>
+ */
 export default function LoggingImpression({ children, impressionOptions, ...rest }: Props) {
   return (
     <LoggingEvent eventName="onImpressionStart" loggerType="impression" {...rest}>
