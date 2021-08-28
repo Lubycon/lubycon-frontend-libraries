aab49405b1a331ed5dd2471c449918c195a50530
HyeIn Ju <hyein.ju92@gmail.com>
2021-08-28 16:44:40 +0900

    feat(react): useClipboardCopy hook 추가

---
+++ packages/react/src/hooks/useClipboardCopy.ts
@@ -0,0 +1,28 @@
+import { useCallback } from 'react';
+
+/**
+ * 매개변수로 주어진 string을 clipboard copy하는 함수를 반환하는 hook입니다.
+ * https환경에서만 사용 가능하며, IE는 지원하지 않습니다.
+ *
+ * @param onCopyCallback 매개변수로 주어진 string 복사 후 실행할 callback 함수
+ * @returns 매개변수로 주어진 string을 복사할 수 있는 함수를 반환합니다.
+ */
+const useClipboardCopy = (onCopyCallback: () => void) => {
+  const copyString = useCallback(
+    async (copyString: string) => {
+      try {
+        await navigator.clipboard.writeText(copyString);
+        if (onCopyCallback !== undefined) onCopyCallback();
+      } catch (error) {
+        throw error;
+      }
+    },
+    [onCopyCallback]
+  );
+
+  return {
+    copyString,
+  };
+};
+
+export default useClipboardCopy;
