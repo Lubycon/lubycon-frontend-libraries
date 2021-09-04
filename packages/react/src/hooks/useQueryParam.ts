e63bf7915ba2e746c9c8984c927ed9d7dc436051
hyunjun kim <93kimhyunjun@gmail.com>
2021-09-04 15:46:23 +0900

    fix: useQueryParam useMemo -> useState and useEffect
    - 기존 로직에서 브라우저의 location에 접근하기 때문에, SSR에서 에러를 발생시킴
    - useState를 이용해서 초기에는 undefined를 내려주고 mounted 이후에 의도하는 상태 할당하도록 변경

--- packages/react/src/hooks/useQueryParam.ts
+++ packages/react/src/hooks/useQueryParam.ts
@@ -1,5 +1,5 @@
 import { parseQueryString } from '@lubycon/utils';
-import { useMemo } from 'react';
+import { useState, useEffect } from 'react';
 
 /**
  * 쿼리스트링의 키를 기반으로 값을 가져옵니다. 만약 값이 존재하지 않을 경우 undefined가 반환됩니다.
@@ -13,15 +13,19 @@ import { useMemo } from 'react';
 export default function useQueryParam<T extends string = string>(key: string): T | undefined;
 export default function useQueryParam<T>(key: string, parser: (value: string) => T): T | undefined;
 export default function useQueryParam<T = string>(key: string, parser?: (value: string) => T) {
-  return useMemo(() => {
+  const [queryParam, setQueryParam] = useState<string | T | undefined>(undefined);
+
+  useEffect(() => {
     const queryString = location != null ? location.search : '';
     const query = parseQueryString(queryString);
 
     const value = query[key];
-    if (value == null) {
-      return undefined;
+    if (typeof value === 'string') {
+      setQueryParam(parser != null ? parser(value) : value);
+    } else {
+      setQueryParam(undefined);
     }
-
-    return parser != null ? parser(value) : query[key];
   }, []);
+
+  return queryParam;
 }
