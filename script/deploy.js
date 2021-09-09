b212c034bf8cecb4c3724a6e03233c6732f72a06
SONGMINSEOK <somony9292@gmail.com>
2021-09-09 10:33:51 +0900

    chore: typedoc publish deploy setup (#58)

---
+++ script/deploy.js
@@ -0,0 +1,22 @@
+const ghpages = require('gh-pages');
+const path = require('path');
+const fs = require('fs');
+const token = process.env.ACCESS_TOKEN;
+const subdomain = 'fe-lib';
+
+ghpages.publish(
+  path.join(__dirname, '../docs'),
+  {
+    branch: 'main',
+    remote: 'origin',
+    repo: `https://${token}@github.com/Lubycon/${subdomain}.lubycon.io.git`,
+    message: '루비콘 라이브러리 문서 배포',
+  },
+  (err) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log(`🚀  루비콘 라이브러리 문서 배포가 완료되었습니다!`);
+    }
+  }
+);
