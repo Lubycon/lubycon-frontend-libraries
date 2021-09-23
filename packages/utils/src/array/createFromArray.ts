1f757c3a0dc815bff0ab5e9a4ecf903df82eb402
evan-moon <bboydart91@gmail.com>
2021-09-23 14:31:43 +0900

    feat(utils): 동일한 key에 대한 예외처리 추가

--- packages/utils/src/array/createFromArray.ts
+++ packages/utils/src/array/createFromArray.ts
@@ -14,6 +14,10 @@
 export function createMapFromArray<T, K extends keyof T>(array: T[], key: K) {
   const map = new Map<T[K], T>();
   array.forEach((item) => {
+    if (map.get(item[key]) != null) {
+      throw new Error('이미 동일한 key가 존재합니다.');
+    }
+
     map.set(item[key], item);
   });
 
@@ -39,6 +43,10 @@ export function createObjectFromArray<T extends Record<string, any>, K extends k
 ) {
   const obj: Record<string, T> = {};
   array.forEach((item) => {
+    if (obj[item[key]] != null) {
+      throw new Error('이미 동일한 key가 존재합니다.');
+    }
+
     obj[item[key]] = item;
   });
 
