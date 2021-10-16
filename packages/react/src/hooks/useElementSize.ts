871b3d0615ce564124e1bf0d397c4e178c772416
evan-moon <bboydart91@gmail.com>
2021-10-16 16:58:15 +0900

    feat(react): useElementSize 훅 추가

---
+++ packages/react/src/hooks/useElementSize.ts
@@ -0,0 +1,23 @@
+import { RefObject, useMemo } from 'react';
+
+const DEFAULT_CASE = {
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+  x: 0,
+  y: 0,
+  width: 0,
+  height: 0,
+};
+
+export function useElementSize(ref: RefObject<HTMLElement>) {
+  return useMemo(() => {
+    if (!ref.current) {
+      return DEFAULT_CASE;
+    }
+    return ref.current?.getBoundingClientRect();
+  }, [ref.current]);
+}
+
+export default useElementSize;
