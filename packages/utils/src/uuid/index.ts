cd310706b0268b347d60a2d295a925dbb4c0b903
marcus <rhcn258@gmail.com>
2021-10-22 00:25:54 +0900

    feat(utils): UUID 코드리뷰 적용 및 생성 방식 변경

--- packages/utils/src/uuid/index.ts
+++ packages/utils/src/uuid/index.ts
@@ -1,12 +1,24 @@
 /**
- * UUID를 쉽게 생성해주는 함수입니다.
+ * UUID를 생성해주는 함수입니다.
  *
  */
 
 export function generateUUID(): string {
+  // timeStmap
+  let d = new Date().getTime();
+  // 페이지 로드 후 microseconds
+  let d2 = (typeof performance !== 'undefined' && performance.now && performance.now() * 1000) || 0;
+
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-    const r = (Math.random() * 16) | 0,
-      v = c === 'x' ? r : (r & 3) | 8;
-    return v.toString(16);
+    let r = Math.random() * 16;
+    if (d > 0) {
+      r = (d + r) % 16 | 0;
+      d = Math.floor(d / 16);
+    } else {
+      r = (d2 + r) % 16 | 0;
+      d2 = Math.floor(d2 / 16);
+    }
+
+    return (c === 'x' ? r : (r & 0x3) | 0x8).toString(16);
   });
 }
