74c16b0a5bbee079d110e8776f47de345bbbd208
hyunjun kim <93kimhyunjun@gmail.com>
2021-10-25 23:53:33 +0900

    chore(react): useQueryParam 설명 좀 더 명확한 표현으로 변경

--- packages/react/src/hooks/useQueryParam.ts
+++ packages/react/src/hooks/useQueryParam.ts
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react';
 
 /**
  * 쿼리스트링의 키를 기반으로 값을 가져옵니다. 만약 값이 존재하지 않을 경우 undefined가 반환됩니다.
- * SSR을 고려하여 처음에는 undefined를 반환하고 mounted 이후에 해당 값의 존재 여부에 따른 결과 값을 반환합니다.
+ * SSR의 Hydration Mismatch를 방지하기 위해 처음에는 undefined를 반환하고 mounted 이후에 해당 값의 존재 여부에 따른 결과 값을 반환합니다.
  *
  * @example
  * // https://lubycon.io?foo=1&bar=hello
