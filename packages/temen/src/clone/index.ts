8c6b54a5ca6c9e86832b3ce81d05762907ea984c
geonwoo.jeong <doscmdev@gmail.com>
2021-11-09 03:10:29 +0900

    feat: add clone

---
+++ packages/temen/src/clone/index.ts
@@ -0,0 +1,30 @@
+/**
+ * @param {any} value 복제 할 값.
+ * @returns {any} 복제 된 값을 반환.
+ * @example
+ *
+ * const objects = [{ 'a': 1 }, { 'b': 2 }]
+ *
+ * const shallow = clone(objects)
+ * console.log(shallow[0] === objects[0])
+ * // => true
+ */
+
+function clone<T>(value: T): T;
+function clone(value: any) {
+  if (value instanceof Date) {
+    return new Date(value.getTime());
+  }
+
+  if (Array.isArray(value)) {
+    return [...value];
+  }
+
+  if (value instanceof Object) {
+    return { ...value };
+  }
+
+  return value;
+}
+
+export default clone;
