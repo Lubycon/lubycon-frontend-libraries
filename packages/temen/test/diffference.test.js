a742e0c6c0cde3f1d6f72e1549fff96787d7c7a5
evan-moon <bboydart91@gmail.com>
2021-11-12 15:24:19 +0900

    테스트 설명 추가

--- packages/temen/test/diffference.test.js
+++ packages/temen/test/diffference.test.js
@@ -2,13 +2,13 @@ import assert from 'assert';
 import { differenceWith } from '../src/difference';
 
 describe('differenceWith', () => {
-  it('differenceWith 함수는 인자로 받은 두 배열의 차이를 반환할 수 있다.', function () {
+  it('differenceWith 함수는 인자로 받은 두 배열의 차이를 반환할 수 있다. (원시 타입)', function () {
     assert.deepStrictEqual(
       differenceWith([0, 1, 2, 3, 4, 5], [0, 1, 2], (x, y) => x === y),
       [3, 4, 5]
     );
   });
-  it('differenceWith 함수는 인자로 받은 두 배열의 차이를 반환할 수 있다.', function () {
+  it('differenceWith 함수는 인자로 받은 두 배열의 차이를 반환할 수 있다. (복잡한 타입)', function () {
     assert.deepStrictEqual(
       differenceWith(
         [
