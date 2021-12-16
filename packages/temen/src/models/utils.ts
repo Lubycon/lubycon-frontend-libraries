a11e43042529bfa337cac9825be47f20906d81b1
Evan Moon <bboydart91@gmail.com>
2021-12-16 18:07:26 +0900

    feat(temen): unwrap하는 타입 추가 (#114)
    * feat(temen): unwrap하는 타입 추가
    
    * feat(temen): 여러 뎁스도 해결할 수 있도록 수정

--- packages/temen/src/models/utils.ts
+++ packages/temen/src/models/utils.ts
@@ -1,8 +1,23 @@
-/** 제네릭으로 받은 맵의 키를 유지한 채 밸류의 타입을 Value로 변경합니다 */
+/**
+ * 제네릭으로 받은 맵의 키를 유지한 채 밸류의 타입을 Value로 변경합니다
+ */
 export type TypeMap<T, Value> = {
   [K in keyof T]: Value;
 };
 
+/**
+ * 제네릭으로 받은 맵의 키를 유지한 채 밸류의 타입을 nullable하게 변경합니다
+ */
 export type Nullable<T> = {
   [K in keyof T]: T[K] | null;
 };
+
+/**
+ * Promise<T> 타입을 T 타입으로 변환합니다
+ */
+export type UnwrapPromise<T> = T extends PromiseLike<infer U> ? UnwrapPromise<U> : T;
+
+/**
+ * Array<T> 타입을 T 타입으로 변환합니다
+ */
+export type UnwrapArray<T> = T extends Array<infer U> ? UnwrapArray<U> : T;
