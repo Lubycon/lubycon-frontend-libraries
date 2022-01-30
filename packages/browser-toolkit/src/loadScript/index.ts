b175ed4b2df5a1a9c977494a04cd965668181cd2
evan-moon <bboydart91@gmail.com>
2022-01-30 14:31:17 +0900

    feat(browser-toolkit): jsDoc 추가

--- packages/browser-toolkit/src/loadScript/index.ts
+++ packages/browser-toolkit/src/loadScript/index.ts
@@ -2,6 +2,21 @@ interface Options {
   async?: boolean;
   defer?: boolean;
 }
+
+/**
+ * 비동기적으로 스크립트를 로드하고 실행합니다. 로드가 완료되면 Promise가 resolve됩니다.
+ *
+ * 함수의 두 번째 인자로 async, defer 속성을 추가할 수도 있으며, 기본 값은 async = true , defer = true입니다.
+ *
+ * @example
+ * ```ts
+ * function loadGoogleSDK () {
+ *   return loadScript('https://developers.kakao.com/sdk/js/kakao.js');
+ *   // 또는
+ *   return loadScript('https://developers.kakao.com/sdk/js/kakao.js', { async: false, defer: true });
+ * }
+ * ```
+ */
 export function loadScript(src: string, options: Options = {}) {
   const { async = true, defer = true } = options;
   if (window == null || document == null) {
