492a0af587bc48de2e92cbbf22fd3da0a63cdb62
evan-moon <bboydart91@gmail.com>
2022-01-30 14:54:50 +0900

    feat(browser-toolkit): async는 기본 true, defer는 false로 변경

--- packages/browser-toolkit/src/loadScript/index.ts
+++ packages/browser-toolkit/src/loadScript/index.ts
@@ -6,7 +6,7 @@ interface Options {
 /**
  * 비동기적으로 스크립트를 로드하고 실행합니다. 로드가 완료되면 Promise가 resolve됩니다.
  *
- * 함수의 두 번째 인자로 async, defer 속성을 추가할 수도 있으며, 기본 값은 async = true , defer = true입니다.
+ * 함수의 두 번째 인자로 async, defer 속성을 추가할 수도 있으며, 기본 값은 async = true , defer = false입니다.
  *
  * @example
  * ```ts
@@ -15,7 +15,7 @@ interface Options {
  * ```
  */
 export function loadScript(src: string, options: Options = {}) {
-  const { async = true, defer = true } = options;
+  const { async = true, defer = false } = options;
   if (window == null || document == null) {
     console.warn('loadScript 함수는 브라우저가 아닌 환경에서 실행될 수 없습니다.');
     return Promise.resolve();
