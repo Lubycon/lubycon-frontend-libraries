dcf1578a5332926c7ff54f5f9d44d032b0a77114
geonwoo.jeong <doscmdev@gmail.com>
2022-02-02 17:30:17 +0900

    docs: add jsdoc

--- packages/react/src/hooks/useStates.tsx
+++ packages/react/src/hooks/useStates.tsx
@@ -1,11 +1,38 @@
 import { useState } from 'react';
 
-const useStates = <State extends Record<string, unknown>>(initialState: State) => {
+/**
+ * useStates hook
+ * @param initialState
+ * @returns [state, setState]
+ *
+ * @example
+ * ```ts
+ * function Foo() {
+ *   const contact = {
+ *     name: 'gildong',
+ *     number: '010-1234-5678',
+ *   };
+ *
+ *   const [values, update] = useStates(contact);
+ *
+ *   return (
+ *     <div>
+ *       <button onClick={() => update('name', '')}> Clear name</button>
+ *       <p>name: {values.name}</p>
+ *       <button onClick={() => update('number', '')}> Clear number</button>
+ *       <p>name: {values.number}</p>
+ *     </div>
+ *   );
+ * }
+ * ```
+ */
+
+function useStates<State extends Record<string, unknown>>(initialState: State) {
   const [values, setValues] = useState(initialState);
   const update = <Key extends keyof State>(name: Key, value: State[Key]) =>
     setValues((form) => ({ ...form, [name]: value }));
 
   return [values, update] as const;
-};
+}
 
 export default useStates;
