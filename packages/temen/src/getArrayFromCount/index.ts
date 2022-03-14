25c93d2086189f2e8fe2edb540d8c83e45cc810f
evan-moon <bboydart91@gmail.com>
2022-03-14 22:46:38 +0900

    fix(temen): getArrayFromCount의 반환 타입이 unknown[]으로 잡히는 이슈 수정

--- packages/temen/src/getArrayFromCount/index.ts
+++ packages/temen/src/getArrayFromCount/index.ts
@@ -1,3 +1,5 @@
+import { identify } from '../functions';
+
 /**
  * 인자로 받은 숫자만큼의 길이를 가진 배열을 반환합니다. 두 번째 인자의 mapper를 넘기지 않으면, 인덱스를 원소 값으로 가집니다.
  *
@@ -5,6 +7,11 @@
  * getArrayFromCount(5) // [0, 1, 2, 3, 4];
  * getArrayFromCount(5, i => `${i}!`); // ['0!', '1!', '2!', '3!', '4!']
  */
-export default function getArrayFromCount<T>(count: number, mapper?: (i: number) => T) {
-  return Array.from({ length: count }, (_, i) => mapper?.(i) ?? i);
+function getArrayFromCount(count: number): number[];
+function getArrayFromCount<T>(count: number, mapper: (i: number) => T): T[];
+function getArrayFromCount<T>(count: number, mapper?: (i: number) => T) {
+  const array = Array.from<number>({ length: count });
+  return array.map((v) => mapper?.(v) ?? identify(v));
 }
+
+export default getArrayFromCount;
