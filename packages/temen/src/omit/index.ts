e0c53e3fdb857817f1bdc8a7bb9f76c5e7ce5296
evan-moon <bboydart91@gmail.com>
2022-04-09 16:07:29 +0900

    chore: omit 함수 example 추가

--- packages/temen/src/omit/index.ts
+++ packages/temen/src/omit/index.ts
@@ -2,6 +2,11 @@ import getObjectKeys from '../getObjectKeys';
 
 /**
  * 객체를 받아 원하는 키를 제외한 새로운 객체를 반환합니다.
+ *
+ * @example
+ * ```ts
+ * omit({ a: 1, b: '2', c: 3 }, ['a', 'c']); // { b: '2' }
+ * ```
  */
 function omit<T extends Record<string | number | symbol, unknown>, K extends keyof T>(
   object: T,
