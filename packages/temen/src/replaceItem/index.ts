634b8d24cd1d2b259e084a91115699ab3e24fdc0
evan-moon <bboydart91@gmail.com>
2022-07-26 23:12:27 +0900

    replaceItem의 Shallow copy 테스트 추가

--- packages/temen/src/replaceItem/index.ts
+++ packages/temen/src/replaceItem/index.ts
@@ -1,7 +1,7 @@
 import { iif } from '../iif';
 
 /**
- * 배열 내 지정된 인덱스에 위치한 원소를 새로운 원소로 변경하고 새로운 배열을 반환합니다.
+ * 배열 내 지정된 인덱스에 위치한 원소를 새로운 원소로 변경하고 새로운 배열을 반환합니다. 대상 배열은 얕은 복사 되기 때문에 내부 원소들의 레퍼런스는 변하지 않습니다.
  */
 export function replaceItem<T>(arr: T[], targetIndex: number, newItem: T) {
   return arr.map((item, index) => {
--- packages/temen/test/replaceItem.test.js
+++ packages/temen/test/replaceItem.test.js
@@ -6,7 +6,11 @@ describe('replaceItem', () => {
     expect(replaceItem(array, 2, 5)).toStrictEqual([1, 2, 5]);
     expect(replaceItem(array, 2, 5)).not.toBe(array);
   });
-  it('preserveRefReplaceItem 함수는 특정 인덱스의 원소를 업데이트하고 원본 배열을 반환한다..', function () {
+  it('replaceItem 함수는 대상 배열을 얕은 복사한다.', function () {
+    const array = [1, 2, 3];
+    expect(replaceItem(array, 2, 5)[0]).toBe(array[0]);
+  });
+  it('preserveRefReplaceItem 함수는 특정 인덱스의 원소를 업데이트하고 원본 배열을 반환한다.', function () {
     const array = [1, 2, 3];
     expect(preserveRefReplaceItem(array, 2, 5)).toStrictEqual([1, 2, 5]);
     expect(preserveRefReplaceItem(array, 2, 5)).toBe(array);
