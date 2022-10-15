83799f5520cfc9480b282acc3294202f3bc10d18
zi-gae <doscmdev@gmail.com>
2022-10-15 15:50:19 +0900

    feat: add jsdoc

--- packages/react/src/hooks/useIdle.ts
+++ packages/react/src/hooks/useIdle.ts
@@ -1,5 +1,21 @@
 import { useCallback, useEffect, useRef } from 'react';
 
+/**
+ * 사용자의 idle 상태를 감지하여 callback 실행 및 boolean 상태를 반환합니다.
+ *
+ * @param {number} timeout idle 상태를 감지할 시간 (ms)
+ * @param {() => void} onIdle idle 상태가 되었을 때 실행할 callback
+ *
+ * @example
+ * ```tsx
+ * const { isIdle, cancel, reset } = useIdle(1000, () => { console.log('idle') });
+ *
+ * console.log(isIdle); // true or false
+ *
+ * return <div onMouseMove={reset} />
+ * ```
+ */
+
 function useIdle(timeout: number, onIdle: () => void) {
   const timeoutId = useRef<number | null>(null);
   const isIdle = useRef(false);
